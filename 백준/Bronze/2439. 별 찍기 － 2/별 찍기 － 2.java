418b65d19700fc988eda7d491ab13dfe6a71ad1e
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-04-27 20:21:04 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 240 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int iVal1 = scanner.nextInt();
+        String starstxt = "";
+        for (int i = 0; i < iVal1; i++) {
+            starstxt += " ";
+        }
+        for (int i = 1; i <= iVal1; i++) {
+            starstxt += "*";
+            System.out.printf("%s\n",starstxt.substring(i));
+        }
+    }
+}
