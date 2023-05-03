78d4b274c34a1bf42ffce2ddb538db87be3ab655
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-03 13:49:39 +0900

    [Bronze II] Title: 소수 찾기, Time: 224 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputCnt = scanner.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < inputCnt; i++) {
+            if (isPrime(scanner.nextInt())) {
+                cnt += 1;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
