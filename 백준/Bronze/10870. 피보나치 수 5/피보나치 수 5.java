61b561b888369fad4102de7f50682f00f36bdb23
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-08 16:46:53 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 204 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int[] intArr = {0, 1};
+        if(input < 2) {
+            System.out.printf("%d", intArr[input]);
+            return;
+        } else {
+            intArr = new int[input + 1];
+            intArr[0] = 0;
+            intArr[1] = 1;
+
+            for (int i = 2; i <= input; i++) {
+                intArr[i] = intArr[i-2] + intArr[i-1];
+            }
+            System.out.printf("%d", intArr[input]);
+        }
+    }
+}
