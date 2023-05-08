c9d64b12ca5f77468b0905ecbeab154f216e6268
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-08 19:06:15 +0900

    [Bronze III] Title: 이진수, Time: 240 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Bronze/3460. 이진수/이진수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testNumber = scanner.nextInt();
+        for (int i = 0; i < testNumber; i++) {
+            String binaryString = Integer.toBinaryString(scanner.nextInt());
+            String reverse = "";
+            for (int j = binaryString.length() - 1; j >= 0; j--) {
+                reverse += binaryString.charAt(j);
+            }
+
+            for (int j = 0; j < reverse.length(); j++) {
+                if (reverse.charAt(j) == '1') {
+                    System.out.printf("%d ", j);
+                }
+            }
+        }
+    }
+}
