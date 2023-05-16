2fb8c82d097fc32347293ac192f50ef66d22f080
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-16 19:32:45 +0900

    [unrated] Title: rny_string, Time: 0.06 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,13 +1,6 @@
 class Solution {
     public String solution(String rny_string) {
-        String[] my_stringArr = rny_string.split("");
-        String answer = "";
-        for (String character : my_stringArr) {
-            if (character.equals("m")) {
-                character = "rn";
-            }
-            answer += character;
-        }
-        return answer;
+
+        return rny_string.replaceAll("m", "rn");
     }
 }
