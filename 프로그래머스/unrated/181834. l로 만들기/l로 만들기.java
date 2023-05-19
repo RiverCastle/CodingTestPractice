a67e8ec5856754a04b53eaf71611e90547bba81f
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-19 14:19:34 +0900

    [unrated] Title: l로 만들기, Time: 21.35 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for (int i = 0; i < myString.length(); i++) {
-            char character = myString.charAt(i);
-            if (character < 'l') {
-                answer += "l";
-            } else {
-                answer += character;
-            }
-        }
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
