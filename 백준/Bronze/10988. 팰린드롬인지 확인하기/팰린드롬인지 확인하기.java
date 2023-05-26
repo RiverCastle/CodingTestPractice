ad9ce9450a3345543f3cbd2f70a87c634c959f8f
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-26 22:25:31 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> stackStr = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        String[] alphabets = word.split("");
+
+        for (String alphabet : alphabets) {
+            stackStr.push(alphabet);
+        }
+        
+        String answer = "";
+        while (!stackStr.empty()) {
+            answer += stackStr.pop();
+        }
+
+        System.out.println(answer.equals(word) ? 1 : 0);
+    }
+}
