2c1e968c98b474809bdec0acfce8fe3c37f3070f
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-29 01:25:17 +0900

    [unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.12 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class Solution {
+    public Stack<Integer> solution(int[] arr, boolean[] flag) {
+        Stack<Integer> answer = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (flag[i]) {
+                for (int j = 0; j < arr[i] * 2; j++) {
+                    answer.push(arr[i]);
+                }
+            } else {
+                for (int j = 0; j < arr[i]; j++) {
+                    answer.pop();
+                }
+            }
+        }
+        return answer;
+    }
+}
