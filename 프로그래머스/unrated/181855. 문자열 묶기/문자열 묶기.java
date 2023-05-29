617222f025ae23968b65ca9bb7c46e80ea6bce6c
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-29 12:42:29 +0900

    [unrated] Title: 문자열 묶기, Time: 7.38 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(String[] strArr) {
+        int[] answer = new int[30];
+        for (String str : strArr) {
+            answer[str.length() - 1] += 1;
+        }
+
+
+        int max = answer[0];
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] > max) {
+                max = answer[i];
+            }
+        }
+        return max;
+    }
+
+    /*
+    public static void main(String[] args) {
+        Solution181855 solution181855 = new Solution181855();
+        String[] strArr = new String[] {"a","bc","d","efg","hi"};
+        solution181855.solution(strArr);
+    }
+     */
+}
