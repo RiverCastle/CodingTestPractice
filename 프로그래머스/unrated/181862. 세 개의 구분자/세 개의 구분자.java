c80822164b689ae338bf1f5495eac50a39190187
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-29 11:34:20 +0900

    [unrated] Title: 세 개의 구분자, Time: 43.42 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> solution(String myStr) {
+        String[] splitters = new String[] {"a", "b", "c"};
+
+        for (String splitter : splitters) {
+            myStr = myStr.replaceAll(splitter, " ");
+        }
+
+        String[] splitted = myStr.split(" ");
+
+        List<String> answer = new ArrayList<>();
+        
+        for (String element : splitted) {
+            if (!element.equals("")) {
+                answer.add(element);
+            }
+        }
+        if (answer.size() == 0) {
+            answer.add("EMPTY");
+        }
+        
+        return answer;
+    }
+}
