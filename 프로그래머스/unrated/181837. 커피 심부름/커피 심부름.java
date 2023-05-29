319060cb9ea61476630a0dad27c80846635c4094
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-29 15:14:22 +0900

    [unrated] Title: 커피 심부름, Time: 0.05 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] orders) {
+        int answer = 0;
+        for (String order : orders) {
+            if (order.contains("americano")||order.equals("anything")) {
+                answer += 4500;
+            } else {
+                answer += 5000;
+            }
+        }
+        return answer;
+    }
+}
