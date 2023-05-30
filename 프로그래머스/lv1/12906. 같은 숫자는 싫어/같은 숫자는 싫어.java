725f7cb9d6e865021c92870e55e82563d1cebf4e
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-05-30 20:28:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.01 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        Queue queue = new ArrayDeque();
+        int last = arr[0];
+        for (int num : arr) {
+            if (queue.size() == 0) {
+                queue.add(num);
+                last = num;
+            } else {
+                if (num != last) {
+                    queue.add(num);
+                    last = num;
+                }
+            }
+        }
+
+
+        int[] answer = new int[queue.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = (int) queue.poll();
+        }
+        return answer;
+    }
+}
