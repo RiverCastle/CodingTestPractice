38a75ad067d04303f75ae54d9db2d01da79779b1
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-06-02 23:07:56 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 268 ms, Memory: 19956 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int people = scanner.nextInt();
+        int period = scanner.nextInt();
+
+        Queue<Integer> queue = new ArrayDeque();
+        for (int i = 1; i <= people; i++) {
+            queue.add(i);
+        }
+
+        int[] answer = new int[people];
+        int idx = 0;
+        while (queue.size() != 1) {
+            for (int i = 1; i < period; i++) {
+                queue.add(queue.poll());
+            }
+            answer[idx++] = (int) queue.poll();
+        }
+        answer[idx] = (int) queue.poll();
+
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        bufferedWriter.write("<");
+        for (int i = 0; i < answer.length - 1; i++) {
+            bufferedWriter.write(answer[i] + ", ");
+        }
+        bufferedWriter.write(answer[answer.length - 1] + ">");
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
