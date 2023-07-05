3654368f71e2836449a6b3d94007f59f9af7f0d4
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-07-05 09:34:16 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -3,33 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+public class Main {// 블랙잭
+    public int solution() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // 카드 갯수, 목표 숫자
+        StringTokenizer infoToken = new StringTokenizer(reader.readLine());
+        int cardCount = Integer.parseInt(infoToken.nextToken());
+        int target = Integer.parseInt(infoToken.nextToken());
 
-        int cardsNum = Integer.parseInt(stringTokenizer.nextToken());
-        int targetSum = Integer.parseInt(stringTokenizer.nextToken());
-        int[] cards = new int[cardsNum];
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        bufferedReader.close();
-
-        for (int i = 0; i < cardsNum; i++) {
-            cards[i] = Integer.parseInt(stringTokenizer.nextToken());
+        StringTokenizer cardTokens = new StringTokenizer(reader.readLine());
+        int[] cards = new int[cardCount];
+        for (int i = 0; i < cardCount; i++) {
+            cards[i] = Integer.parseInt(cardTokens.nextToken());
         }
+
         int max = 0;
-        int cnt = 0;
-        for (int i = 0; i < cardsNum; i++) {
-            for (int j = i + 1; j < cardsNum; j++) {
-                for (int k = j + 1; k < cardsNum; k++) {
-                    int temp = cards[i] + cards[j] + cards[k];
-                    if (temp <= targetSum && temp >= max) {
-                        max = temp;
+
+        // 3장의 카드를 뽑는다.
+        // 첫번째 카드 -> n 장의 카드가 있다면 첫번째는 n - 2 까지
+        for (int i = 0; i < cardCount - 2; i++) {
+            // 두번째 카드 -> 두번째는 n - 1 까지
+            for (int j = i + 1; j < cardCount - 1; j++) {
+                // 세번째 카드 -> 세번째는 n 까지
+                for (int k = j + 1; k < cardCount; k++) {
+                    //System.out.println("선택한 카드 출력");
+                    // TODO
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if (sum <= target && max < sum) {
+                        max = sum;
                     }
                 }
             }
         }
-        System.out.println(max);
+        return max;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(new Main().solution());
     }
 }
