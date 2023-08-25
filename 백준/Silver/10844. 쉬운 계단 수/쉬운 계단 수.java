9a92d3c4ebc30cd538bb4fd501179893551edc7d
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-08-25 14:29:59 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,42 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {// 쉬운 계단수
-    static long [][] dp;
+public class Main {
+    public static int[][] answerArr;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
-        int n = Integer.parseInt(br.readLine());
-        dp = new long[n+1][10];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int numberN = Integer.parseInt(bufferedReader.readLine());
+        answerArr = new int[numberN][10];
+        int divisor = 1000000000;
 
-        for(int i=0; i<=9; i++){
-            dp[1][i]=1l;
+        for (int i = 0; i < 10; i++) {
+            answerArr[0][i] = 1;
         }
-        long result=0;
-
-        for(int i=1; i<=9; i++){
-            result+=solve(n, i);
-        }
-
-        System.out.println((result)%1000000000);
-    }
-
-    private static long solve(int index, int num){
-        if(index==1){
-            return dp[index][num];
-        }
-
-        if(dp[index][num]==0){
-            if(num==0){
-                dp[index][num] = solve(index-1, 1);
+        for (int i = 1; i < numberN; i++) {
+            answerArr[i][0] = answerArr[i - 1][1] % divisor;
+            for (int j = 1; j < 9; j++) {
+                answerArr[i][j] = (answerArr[i - 1][j - 1] % divisor + answerArr[i - 1][j + 1] % divisor) % divisor;
             }
-            else if(num==9){
-                dp[index][num] = solve(index-1, 8);
-            }
-            else{
-                dp[index][num] = solve(index-1,num+1)+solve(index-1, num-1);
-            }
+            answerArr[i][9] = answerArr[i - 1][8] % divisor;
         }
 
-        return dp[index][num]%1000000000;
+        int answer = 0;
+        for (int i = 1; i < 10; i++) {
+            answer = (answer + answerArr[numberN - 1][i]) % divisor;
+        }
+        System.out.println(answer);
     }
 }
