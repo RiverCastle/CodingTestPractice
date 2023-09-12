063761fb8d2e0ff0540f6d9a7a3e80cff8fc679e
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2023-09-12 12:08:39 +0900

    [Gold IV] Title: 좋다, Time: 208 ms, Memory: 15084 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -3,16 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {// 좋다 https://www.acmicpc.net/problem/1253
+public class Main {// 좋다 해결 https://www.acmicpc.net/problem/1253
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int numberN = Integer.parseInt(bufferedReader.readLine());
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-
-        for (int i = 0; i < numberN; i++) priorityQueue.add(Integer.parseInt(stringTokenizer.nextToken()));
         int[] intArr = new int[numberN];
-        for (int i = numberN - 1; i >= 0; i--) intArr[i] = priorityQueue.poll();
+        for (int i = numberN - 1; i >= 0; i--) intArr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        Arrays.sort(intArr);
+//        System.out.println("Arrays.toString(intArr) = " + Arrays.toString(intArr));
 
         int answer = 0;
         for (int i = 0; i < numberN; i++) {
@@ -22,15 +21,16 @@ public class Main {// 좋다 https://www.acmicpc.net/problem/1253
             while (pointer1 != pointer2) {
                 if (pointer1 == i) pointer1++;
                 if (pointer2 == i) pointer2--;
-                if (pointer2 <= pointer1) break; // 혹시 몰라서
+                if (pointer2 <= pointer1) break;
                 int sum = intArr[pointer1] + intArr[pointer2];
-
+//                System.out.printf("%d %d %d %d \n", target, sum, intArr[pointer1], intArr[pointer2]);
                 if (target == sum) {
 //                    System.out.println("***GOOD***");
+//                    System.out.println("answer = " + answer);
                     answer++;
                     break;
-                } else if (sum > target) pointer1++;
-                else if (sum < target) pointer2--;
+                } else if (sum > target) pointer2--;
+                else if (sum < target) pointer1++;
             }
         }
         System.out.println(answer);
