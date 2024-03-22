875a0bf2575b8d8a22a37bb91cdb938639854371
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2024-03-22 11:11:55 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,23 @@
+-- 코드를 작성해주세요
+WITH GRADE AS (
+    SELECT EMP_NO, 
+        CASE
+            WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+            ELSE 'C'
+        END AS GRADE
+    FROM HR_GRADE 
+    GROUP BY EMP_NO)
+    
+SELECT G.EMP_NO, E.EMP_NAME, G.GRADE, 
+    CASE 
+        WHEN G.GRADE = 'S' THEN E.SAL * 0.2
+        WHEN G.GRADE = 'A' THEN E.SAL * 0.15
+        WHEN G.GRADE = 'B' THEN E.SAL * 0.1
+        ELSE 0
+    END AS BONUS
+FROM HR_EMPLOYEES E
+INNER JOIN GRADE G
+ON G.EMP_NO = E.EMP_NO
+ORDER BY EMP_NO;
