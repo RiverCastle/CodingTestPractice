4277d3960bc9ec7384439105f281b7fdccf1c5fd
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2024-03-26 09:55:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int n) {
         String temp = Integer.toString(n, 3);
-        StringBuilder sb = new StringBuilder();
-        for (int i = temp.length() - 1; i >= 0; i--)
-            sb.append(temp.charAt(i));
-
+        StringBuilder sb = new StringBuilder(temp).reverse();
         int answer = Integer.parseInt(sb.toString(), 3);
         return answer;
     }
