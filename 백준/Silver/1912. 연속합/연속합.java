b00ad3efc64307605761f9a65b7ad99c0673aa55
RiverCastle <131141755+RiverCastle@users.noreply.github.com>
2024-11-28 14:10:32 +0900

    [Silver II] Title: 연속합, Time: 244 ms, Memory: 24140 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,28 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        // 입력받기
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int numberN = Integer.parseInt(br.readLine());
-        String[] inputs = br.readLine().split(" ");
-        int[] numbers = new int[numberN];
-        for (int i = 0; i < numberN; i++) numbers[i] = Integer.parseInt(inputs[i]);
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 
-        // 연속합 구하기
-        int max = numbers[0];
-        int sum = numbers[0];
+        int[] arr = new int[numberN];
+        int answer = Integer.parseInt(stringTokenizer.nextToken());
+        int candidate = answer;
+
         for (int i = 1; i < numberN; i++) {
-            int presentValue = numbers[i];
-            sum += presentValue;
-            if (sum > max) max = sum; //현재 수를 더했을 때, 기존 max보다 클 때,
-            else if (presentValue > sum) { // 현재 수가 기존 합보다 클 때, 
-                sum = presentValue;
-                if (sum > max) max = sum; // 합이 max 보다 클 때, 
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+            int subSum = Math.max(arr[i], candidate + arr[i]);
+            candidate = Math.max(subSum, candidate);
+            if (answer >= candidate) {
+                candidate = Math.max(arr[i], subSum );
+            } else {
+                answer = candidate;
             }
         }
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
